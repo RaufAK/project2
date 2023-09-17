578754269f059faac66161e8f456328aed661b3d
Zachary <125631990+ZachPegram@users.noreply.github.com>
2023-09-17 16:31:34 -0400

    The function FilterCriteria Is done to what I could complete.

--- make-multi-filter.js
+++ make-multi-filter.js
@@ -1,4 +1,27 @@
 'use strict';
-function filterCriteria(){}
+
+var currentArray = [1,2,3];
+function filterCriteria(element){
+
+
+    // Probably a more proper way of going through indexes.
+    for(let i = 0; i < currentArray.length-1; i++){
+        if (element !== currentArray[i]){
+            delete currentArray[i];
+        }
+    }
+
+
+    //If the array has a set number of indexes
+    // if(element !== 1) {
+    //     delete currentArray[1];
+    // }else if(element !== 2){
+    //     delete currentArray[2];
+    // }else if(element !== 3){
+    //     delete currentArray[3];
+    // }else {
+    //     return currentArray;
+    // }
+}
 
 function callback() {}
